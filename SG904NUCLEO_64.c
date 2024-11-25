c014f81ce8570aa7ad86551e60233316758db4d2
JvRosmano <jv_ros@hotmail.com>
2024-11-25 03:12:45 -0300

    SG90 API
    API with funcions to use SG90 Motor

---
+++ SG904NUCLEO_64.c
@@ -0,0 +1,49 @@
+/*
+ *  SG904NUCLEO_64.h
+ *  Copyright (C) 2024
+ *  João Vitor de M.G. Rosmaninho <jvrosmaninho@ufmg.br>,
+ *  Lucas Soares de Salles <lucas-ss-salles@ufmg.br>,
+ *  Stephanie Costa de Avelar <stephanieavelar@ufmg.br>
+ *
+ *  Version 1.0 - API with the following implemented function:
+ *  void SG90_SetPWM(TIM_HandleTypeDef timer, uint32_t channel, uint16_t period, uint16_t pulse);
+ *
+ *  Based on notes from professor Ricardo O. Duarte <ricardoduarte@ufmg.br> for embedded systems programming course.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Created on: 18 de nov de 2024
+ *  Institution: UFMG
+ *  This API contain functions to provide use of some hardware resources
+ *  from Motor shield 293D driver available at:
+ *  https://www.makerhero.com/produto/motor-shield-l293d-driver-ponte-h-para-arduino/
+ */
+
+#include "SG904NUCLEO_64.h"
+#include "main.h"
+
+void SG90_SetPWM(TIM_HandleTypeDef timer, uint32_t channel, uint16_t period, uint16_t pulse){
+	// Configura registradores para uso do PWM
+	HAL_TIM_PWM_Stop(&timer, channel);
+	TIM_OC_InitTypeDef sConfigOC;
+	timer.Init.Period = period;
+	HAL_TIM_PWM_Init(&timer);
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = pulse;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	HAL_TIM_PWM_ConfigChannel(&timer, &sConfigOC, channel);
+	HAL_TIM_PWM_Start(&timer, channel);
+}
